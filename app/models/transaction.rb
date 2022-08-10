c938923cf955f3ce4c35137179de9a87f4a043f7
amrendrakind <vision2010@gmail.com>
2022-08-10 21:01:02 +0530

    Update: Model and migration

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
     belongs_to :user
     belongs_to :group
+
+    validates:name, presense:true
+    validates:amount, presense:true, numericality {greater_than_or_equal_to: 0 }
+
 end
