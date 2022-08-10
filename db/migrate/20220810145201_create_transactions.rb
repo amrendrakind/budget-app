509a901656c512827869cd0976ca4cb5a034b3fb
amrendrakind <vision2010@gmail.com>
2022-08-10 20:47:49 +0530

    Update: Model and migration

--- db/migrate/20220810145201_create_transactions.rb
+++ db/migrate/20220810145201_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
       t.text :name
-      t.float :float default 0.0
+      t.float :float, default: 0.0
       
       t.timestamps
     end
