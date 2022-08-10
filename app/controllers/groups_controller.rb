364ef38632f5c1f6bd5c9c8d204b4b79b9785eab
amrendrakind <vision2010@gmail.com>
2022-08-11 03:47:58 +0530

    Update: Group controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,28 @@
 class GroupsController < ApplicationController
-  def index; end
+  def index
+    @groups = Group.where(user_id: current_user.id)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+  
+    if @group.save
+      redirect_to group_path(@group)
+    else
+      redirect_to new_groups_path(@group)
+    end
+  end
+
+  def destroy
+    @group.destroy
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'group/index'
-  get 'transactions/index'
-  get 'users/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'users#index'
+  resources :users
+  resources :groups do
+    resources :transactions
+  end
+
 end
