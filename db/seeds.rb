64296863eb67d273010ced61af79f2cd23806f80
amrendrakind <vision2010@gmail.com>
2022-08-12 02:46:44 +0530

    Seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 @user = User.create(name: 'Amrendra Kumar', email: 'amrendrak.ind@gmail.com', password: '123456')
 
-@transaction1 = Trading.create(  author_id: @user.id,  name: 'Fruits',  amount: 56)
-@transaction2 = Trading.create(  author_id: @user.id,  name: 'Vegetable',  amount: 12)
-@transaction3 = Trading.create(  author_id: @user.id,  name: 'Books',  amount: 345)
+@group1= Group.create(user_id: @user.id,  name: 'Icon1',  icon: 'app/assets/images/budget_logo.png')
+@group2= Group.create(user_id: @user.id,  name: 'Icon2',  icon: 'app/assets/images/budget_logo.png')
+@group3= Group.create(user_id: @user.id,  name: 'Icon3',  icon: 'app/assets/images/budget_logo.png')
 
-
+@transaction1 = Transaction.create(user_id: @user.id,  name: 'Fruits',  amount: 60, group_id: @group1.id)
+@transaction2 = Transaction.create(user_id: @user.id,  name: 'Vegetable',  amount: 40, group_id: @group2.id)
+@transaction3 = Transaction.create(user_id: @user.id,  name: 'Books',  amount: 100, group_id: @group3.id)
