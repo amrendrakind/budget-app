52d2c9b6e370c3304aa8bd52778f033f8598f759
amrendrakind <vision2010@gmail.com>
2022-08-12 00:03:57 +0530

    Add Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = params[:user_id] # current_user
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   has_many :groups, dependent: :destroy
   has_many :transactions, dependent: :destroy
 
-  validates :name, presense: true
+  validates :name, presence: true
 end
